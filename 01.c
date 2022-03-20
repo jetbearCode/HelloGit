92d25526ca787723ccad1e544e6a8d70064ee8ce
jetbearCode <101528506+jetbearCode@users.noreply.github.com>
2022-03-20 20:51:20 +0800

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float x, y;
+    int z;
+    
+    scanf("%f %f %d", &x, &y,&z);
+    printf("%f\n", trunc( x * y * pow(10, z) ) / pow(10.0, z) );
+    return 0;
+}
